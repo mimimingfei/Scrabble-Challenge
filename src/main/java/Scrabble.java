fd587c48cc808eab42fdfda32a07f8fdd6a7430e
[mimimingfei] <[mingfeiwang950@gmail.com]>
2023-02-13 10:39:49 +0000

    .

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -8,7 +8,7 @@ public class Scrabble {
     private Character[] douLetter;
     private Character[] triLetter;
     private Boolean isDouble, isTriple;
-    private HashMap<Character, Integer> values = new HashMap<>();
+    public HashMap<Character, Integer> values = new HashMap<>();
 
     public Scrabble(String word) {
         this.word = word;
@@ -23,37 +23,38 @@ public class Scrabble {
     }
 
 
-
     public int score() {
         String character = "AEIOULNRSTDGBCMPFHVWYKJXQZ";
         int[] value = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 8, 8, 10, 10};
         for (int i = 0; i < character.length(); i++) {
             values.put(character.charAt(i), value[i]);
         }
-
         int score = 0;
         this.word = word == null ? "" : word.toUpperCase();
-        this.douLetter = douLetter == null ? new Character[0] : douLetter;
-        this.triLetter = triLetter == null ? new Character[0] : triLetter;
-
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             score += values.get(c);
         }
-    score = addLetterScore(score);
-    score = addWordScore(score);
+        score = addDoubleScore(score);
+        score = addTripleScore(score);
+        score = addWordScore(score);
         return score;
 }
     private int addWordScore(int score){
         return score *= (isTriple != null && isTriple.booleanValue() ? 3 : (isDouble != null && isDouble.booleanValue() ? 2 : 1));
     }
-    private int addLetterScore(int score) {
+    private int addDoubleScore(int score) {
+        this.douLetter = douLetter == null ? new Character[0] : douLetter;
         for (Character doubleLetter : douLetter) {
             if (word.indexOf(doubleLetter) != -1) {
-                    int doubleLetterValue = values.get(doubleLetter);
-                    score += doubleLetterValue;
-                }
+                int doubleLetterValue = values.get(doubleLetter);
+                score += doubleLetterValue;
             }
+        }
+        return score;
+    }
+    private int addTripleScore(int score) {
+        this.triLetter = triLetter == null ? new Character[0] : triLetter;
         for (Character tripleLetter : triLetter) {
             if (word.indexOf(tripleLetter) != -1) {
                     int tripleLetterValue = values.get(tripleLetter);
